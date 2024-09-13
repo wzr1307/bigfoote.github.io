f9cc42dbe29e8436bcb90d4f4e9176ba280c0fec
wzr1307 <94065069+wzr1307@users.noreply.github.com>
2024-09-13 19:37:10 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const signInButton = document.querySelector('.sign-in-htm .button');
+const signInButton = document.querySelector('.sign-in-htm .group .button');
 
 signInButton.addEventListener('click', () => {
     window.location.href = 'components/mainMenu/mainMenu.html'; // Замените 'mainMenu.html' на правильное имя вашей страницы
