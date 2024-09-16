8bd2e991e6c0c8e706d0f032280a21ee01564a6f
wzr1307 <94065069+wzr1307@users.noreply.github.com>
2024-09-16 16:43:18 +0300

    Create MainMenu.js

---
+++ components/mainMenu/MainMenu.js
@@ -0,0 +1,4 @@
+function redirectToAnotherPage() {
+    // Замените 'путь_к_другой_странице' на фактический путь
+    window.location.href = 'components/game/game.html'; // Замените 'mainMenu.html' на правильное имя вашей страницы
+}
