25334c854b15ee262c08e36c4eb0ce52271ecef6
wzr1307 <94065069+wzr1307@users.noreply.github.com>
2024-09-17 11:58:19 +0300

    Update mainMenu.js

--- components/mainMenu/mainMenu.js
+++ components/mainMenu/mainMenu.js
@@ -1,4 +1,4 @@
-function redirectToAnotherPag() {
+function redirectToAnother() {
     // Замените 'путь_к_другой_странице' на фактический путь
-    window.location.href = 'components/game/game.html'; // Замените 'mainMenu.html' на правильное имя вашей страницы
+    window.location.href = 'components/game/games.html'; // Замените 'mainMenu.html' на правильное имя вашей страницы
 }
