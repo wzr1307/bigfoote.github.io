5353377d9ee77b93a4250adbd6d402f8557647a5
wzr1307 <94065069+wzr1307@users.noreply.github.com>
2024-10-27 19:53:19 +0300

    Update gameMap.js

--- components/mainMenu/gameMap.js
+++ components/mainMenu/gameMap.js
@@ -1,29 +1,15 @@
-// Загружаем данные карты
+// gameMap.js
+const svg = d3.select("#map");
+
+// Загрузка данных
 d3.json("DANIA.json").then(function(data) {
-    // Выбираем элемент SVG
-    const svg = d3.select("#map");
+  // Преобразование топологических данных в геометрические
+  const danishRegions = topojson.feature(data, data.objects.danmark);
 
-    // Определяем размеры SVG
-    const width = 960;
-    const height = 500;
-    svg.attr("width", width)
-       .attr("height", height);
-
-    // Создаем проекцию карты (например, Mercator)
-    const projection = d3.geoMercator()
-        .scale(150)
-        .center([12, 56])
-        .translate([width / 2, height / 2]);
-
-    // Создаем геопаथ для рисования карты
-    const path = d3.geoPath()
-        .projection(projection);
-
-    // Рисуем карту
-    svg.append("path")
-        .datum(topojson.feature(data, data.objects.danmark))
-        .attr("d", path)
-        .attr("fill", "lightblue")
-        .attr("stroke", "black")
-        .attr("stroke-width", 0.5);
+  // Создание SVG элементов для каждого региона
+  svg.selectAll("path")
+    .data(danishRegions.features)
+    .enter()
+    .append("path")
+    .attr("d", d3.geoPath());
 });
